a0b25d71fd96f55775c98d892728a6e055472d16
Duta Gabi <dutagabryel@gmail.com>
2024-03-18 21:08:41 +0200

    use chained operations for create request

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ function renderTeams(teams) {
 }
 
 function loadTeams() {
-  const promise = fetch("http://localhost:3000/teams-json")
+    fetch("http://localhost:3000/teams-json")
     .then(r => r.json())
     .then(teams => {
       allTeams = teams;
@@ -97,8 +97,6 @@ function onSubmit(e) {
       }
     });
   } else {
-    createTeamRequest(team);
-    window.location.reload();
     const req = createTeamRequest(team);
     const response = req.then(r => r.json());
     response.then(status => {
@@ -106,6 +104,13 @@ function onSubmit(e) {
         window.location.reload();
       }
     });
+    createTeamRequest(team)
+      .then(r => r.json())
+      .then(status => {
+        if (status.success) {
+          window.location.reload();
+        }
+      });
   }
 }
 
